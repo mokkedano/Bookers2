57f07b5229ec77995f601c6acf447fdf14503830
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-03-19 04:06:43 +0000

    [Commit] 4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,12 +15,11 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
       render :index
     end
-    flash[:notice] = "You have created book successfully."
   end
 
 
@@ -39,11 +38,10 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
       render :edit
     end
-    flash[:notice] = "You have updated book successfully."
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,11 +25,10 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user.id)
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
       render :edit
     end
-    flash[:notice] = "You have updated user successfully."
   end
 
 
